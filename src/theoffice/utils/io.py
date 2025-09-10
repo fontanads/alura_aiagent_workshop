2a5591c6b161ee2230761d05ae90510f2e59b512
fontanads <fontanads@gmail.com>
2025-09-10 19:23:07 +0100

    adding class 102

--- src/theoffice/utils/io.py
+++ src/theoffice/utils/io.py
@@ -1,6 +1,8 @@
 import os
 import yaml
 
+from pathlib import Path
+from langchain_community.document_loaders import PyMuPDFLoader
 
 def read_yaml(path):
     with open(path, "r", encoding="utf-8") as file:
@@ -16,3 +18,19 @@ def write_env_from_dict(env_vars: dict, overwrite: bool = True) -> None:
 def update_env_secrets(path_to_secrets: str):
     secrets = read_yaml(path_to_secrets)
     write_env_from_dict(secrets)
+
+
+def read_pdf_files(path_to_pdfs: str):
+    
+    pdfs = []
+    for filename in Path(path_to_pdfs).glob("*.pdf"):
+        print("Loading file:", filename)
+        try:
+            loader = PyMuPDFLoader(filename)
+            pdfs.extend(loader.load())
+        except Exception as e:
+            print("Error loading file:", filename)
+            print(e)
+            continue
+    return pdfs
+        
